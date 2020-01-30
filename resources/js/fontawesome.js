effb26ac54b0fa827640721c8eddca23fd9de622
raiyaliza <raiyalizamm@gmail.com>
2020-01-30 16:16:49 +0500

    Add vote controls to question and answers items

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,5 @@
+import fontawesome from '@fortawesome/fontawesome'
+import solid from '@fortawesome/fontawesome-free-solid'
+
+// Add all icons to the library so you can use it in your page
+fontawesome.library.add(solid.faCaretUp, solid.faCaretDown, solid.faCheck, solid.faStar)
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -6,7 +6,8 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
+              <div class="card-body">
+                <div class="card-title">
                   <div class="d-flex align-items-center">
                     <h1>{{ $question->title }}</h1>
                     <div class="ml-auto">
@@ -14,27 +15,45 @@
                     </div>
                   </div>
                 </div>
-
-                <div class="card-body">
-                  {{-- Wth is this? We're making an accessor for this --}}
-                  {{-- Got it! --}}
+                <hr>
+                <div class="media">
+                  {{-- vote controls --}}
+                  <div class="d-flex flex-column vote-controls">
+                    <a title="This question is useful" class="vote-up">
+                      {{-- using font awesome --}}
+                      <i class="fas fa-caret-up fa-3x"></i>
+                    </a>
+                    <span class="votes-count">123</span>
+                    <a title="This question is not useful" class="vote-down off">
+                      <i class="fas fa-caret-down fa-3x"></i>
+                    </a>
+                    <a title="Click to mark as favorite (Click again to undo)" class="favorite mt-3 favorited">
+                      <i class="fas fa-star fa-3x"></i>
+                    </a>
+                    <span class="favorites-count mt-1">123</span>
+                  </div>
                   {{-- question body --}}
-                  {!! $question->body_html !!}
+                  <div class="media-body">
+                    {{-- Wth is this? We're making an accessor for this --}}
+                    {{-- Got it! --}}
+                    {{-- question body --}}
+                    {!! $question->body_html !!}
 
-                  {{-- user information --}}
-                  <div class="float-right">
-                    <span class="text-muted"> {{ $question->created_date }} </span>
-                    <div class="media mt-2">
-                      <a href="{{ $question->user->url }}" class="pr-2">
-                        <img src="{{ $question->user->avatar }}" alt="">
-                      </a>
-                      <div class="media-body mt-1">
-                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                    {{-- user information --}}
+                    <div class="float-right">
+                      <span class="text-muted"> {{ $question->created_date }} </span>
+                      <div class="media mt-2">
+                        <a href="{{ $question->user->url }}" class="pr-2">
+                          <img src="{{ $question->user->avatar }}" alt="">
+                        </a>
+                        <div class="media-body mt-1">
+                          <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                        </div>
                       </div>
                     </div>
                   </div>
-
                 </div>
+              </div>
             </div>
         </div>
     </div>
@@ -49,7 +68,24 @@
             </div>
             <hr>
             @foreach ($question->answers as $answer)
+
+              {{-- answer vote controls --}}
               <div class="media">
+                <div class="d-flex flex-column vote-controls">
+                  <a title="This answer is useful" class="vote-up">
+                    {{-- using font awesome --}}
+                    <i class="fas fa-caret-up fa-3x"></i>
+                  </a>
+                  <span class="votes-count">1230</span>
+                  <a title="This answer is not useful" class="vote-down off">
+                    <i class="fas fa-caret-down fa-3x"></i>
+                  </a>
+                  <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                    <i class="fas fa-check fa-2x"></i>
+                  </a>
+                </div>
+
+                {{-- answer body --}}
                 <div class="media-body">
                   {{-- answer body --}}
                   {!! $answer->body_html !!}
